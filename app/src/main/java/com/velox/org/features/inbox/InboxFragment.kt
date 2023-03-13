d914cfabfa1b0cb2542175136c11e66927d6df1a
john jake talledo <ronvietiongzon00@gmail.com>
2023-03-13 12:56:44 +0800

    nav home

---
+++ app/src/main/java/com/velox/org/features/inbox/InboxFragment.kt
@@ -0,0 +1,6 @@
+package com.velox.org.features.inbox
+
+import com.velox.org.bases.BaseFragment
+import com.velox.org.databinding.FragmentInboxBinding
+
+class InboxFragment : BaseFragment<FragmentInboxBinding>(FragmentInboxBinding::inflate)
---
+++ app/src/main/java/com/velox/org/features/profile/ProfileFragment.kt
@@ -0,0 +1,6 @@
+package com.velox.org.features.profile
+
+import com.velox.org.bases.BaseFragment
+import com.velox.org.databinding.FragmentProfileBinding
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate)
